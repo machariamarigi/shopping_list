b3e320810f882c4d4298b6d6ca12a6b419a65877
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 18:39:38 +0300

    [Chore #150630956] Add test for a user model

---
+++ tests/test_models.py
@@ -0,0 +1,20 @@
+"""Module for testing application's models"""
+
+from unittest import TestCase
+
+from app.models import User
+
+
+class TestUserModel(TestCase):
+    """Class containing tests for a user"""
+
+    def setUp(self):
+        self.user_instance = User('mash', 'mash@mash.com', 'mash_pass')
+
+    def test_user_get_details(self):
+        """Method to test if user instantiates correctly"""
+        self.assertEqual(self.user_instance.get_details(), {
+            "username": "mash",
+            "email": "mash@mash.com",
+            "password": "mash_pass"
+        })
