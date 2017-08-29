26e2ab61c64c44d63a0cda422bd4edbdb042c883
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 10:20:26 +0300

    [Chore #150614670] Add the genrator function for our flask application

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+
+""" this module deals with the creation of the application"""
+
+from flask import Flask
+
+from ..config import app_config
+
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+
+    return app
