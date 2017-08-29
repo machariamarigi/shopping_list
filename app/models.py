de470b0b7df604ea229080557c2dcab8f17e00cd
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 19:03:13 +0300

    [Chore #150630956] Test if we can create a user using Storage model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 """
-    Module for the application's User, ShoppingList, ShoppingItems and Storage 
+    Module for the application's User, ShoppingList, ShoppingItems and Storage
     models
 """
 
@@ -23,3 +23,28 @@ class User():
 
     def get_details(self):
         return self.details
+
+
+class Storage():
+    """Class will handle volatile storage of the application's data"""
+
+    users = [{
+        'email': None,
+        'password': None,
+        'username': None,
+        'shopping_list': None,
+        'id': 0
+    }]
+
+    def add_user(self, username, email, password):
+        """Method to register users to the application"""
+
+        new_user = User(username, email, password)
+        new_user_details = new_user.get_details()
+        for user in self.users:
+            if new_user_details['email'] == user['email']:
+                break
+                return False
+            else:
+                new_user_details['id'] = len(self.users)
+                self.users.append(new_user_details)
