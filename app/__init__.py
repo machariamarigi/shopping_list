9d45fc5da4403c46d157315d2c6c6fb195503fef
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 22:05:21 +0300

    [Chore #150630956]Add flask_bootsrap library

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 """ this module deals with the creation of the application"""
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 from config import app_config
-from models import Storage
+from .models import Storage
 
 store = Storage()
 
@@ -11,6 +12,8 @@ store = Storage()
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
+
+    Bootstrap(app)
 
     from .landing_page import landing_page as landing_page_blueprint
     app.register_blueprint(landing_page_blueprint)
