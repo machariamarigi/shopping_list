c8c3bf344bf90bf838ae11add6b06bb8fc3546b0
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 21:41:10 +0300

    [Chore #150630956]Add the view renderes for the forms

---
+++ app/auth/views.py
@@ -0,0 +1,46 @@
+
+"""Module handle authentification views"""
+
+from flask import render_template, flash, redirect, url_for, session
+
+from . import auth
+from .forms import SignUpForm, LoginForm
+from app import store
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """Method to handle views for loging in"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        for user in store.users:
+            if form.email.data == user['email']:
+                if form.password.data == user['password']:
+                    logged_in_user = user
+                    session['logged_in'] = True
+                    return redirect(
+                        url_for(
+                            'profile.profilepage',
+                            logged_in_user=logged_in_user['username']))
+
+    return render_template('auth/login.html', title='login', form=form)
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    """Method to handle sign up of users"""
+    form = SignUpForm()
+    if form.validate_on_submit():
+        # TODO: Hash passwords
+        # hash_password = generate_password_hash(form.password.data)
+        store.add_user(form.username.data, form.email.data, form.password.data)
+        flash('You have successfully registered! You may now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', form=form, title='Sign Up')
+
+
+@auth.route('/logout')
+def logout():
+    """Log out a user"""
+    session.pop('logged_in', None)
+    return redirect(url_for('home.homepage'))
