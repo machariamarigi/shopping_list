74c60b4446a2367357bbff90091224dfad5a9e24
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 18:57:36 +0300

    [Chore #150630956] Test if we can create a user using Storage model

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 
-from app.models import User
+from app.models import User, Storage
 
 
 class TestUserModel(TestCase):
@@ -19,3 +19,18 @@ class TestUserModel(TestCase):
             "password": "mash_pass",
             "shopping_lists": []
         })
+
+
+class TestStorage(TestCase):
+    """Class to test storage model"""
+
+    def setUp(self):
+        self.test_store = Storage()
+
+    def test_create_user(self):
+        """Test if we can add new users into the system"""
+        initial_users = len(self.test_store.users)
+        self.test_store.add_user('test', 'test@test.com', 'test')
+        final_users = len(self.test_store.users)
+        self.assertEquals(
+            1, final_users-initial_users, 'User not created')
