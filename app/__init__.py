bfbdd591b310748617a032ac35b2a4da4f9dcb38
Macharia Marigi <machariamarigi@gmail.com>
2017-08-29 11:01:26 +0300

    [Chore #150614670] Register landing_page blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-
 """ this module deals with the creation of the application"""
 
 from flask import Flask
@@ -9,5 +8,8 @@ from config import app_config
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
+
+    from .landing_page import landing_page as landing_page_blueprint
+    app.register_blueprint(landing_page_blueprint)
 
     return app
